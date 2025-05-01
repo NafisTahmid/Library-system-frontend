859285b1779863a38771566c17c77bbc0e424261
sazzadtomal <sazzad.alam@accelx.net>
2025-05-01 13:40:48 +0600

    Second commit

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import {React, useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
-import books from '../books';
+// import books from '../books';
 import Book from '../components/Book';
+import axios from 'axios';
 
 function HomeScreen() {
+  const [books, setBooks] = useState([]);
+  
+  useEffect(() => {
+    async function getBooks() {
+      const { data } = await axios.get(" http://127.0.0.1:8000/api/books/");
+      setBooks(data);
+    };
+    getBooks()
+  },[])
   return (
     <div>
       <Row>
